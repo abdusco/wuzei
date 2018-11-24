c0604655e0b41e19bda47cc963ee8d99be4c633b
Abdussamet Kocak <sssmmt@gmail.com>
2018-11-24 08:29:27 +0300

    Handle error when config file not found Accept wuzei.ini as default config file

--- wuzei/__main__.py
+++ wuzei/__main__.py
@@ -14,21 +14,30 @@ def get_parser() -> ArgumentParser:
     parser.add_argument('config_file',
                         action='store',
                         help='path to config.ini file',
-                        nargs='?',
-                        default='./config.ini')
+                        nargs='?')
     return parser
 
 
 def main():
+    config_paths = ['wuzei.ini', 'config.ini']
     parser = get_parser()
     args = parser.parse_args()
-    config_path = Path(args.config_file)
-    if not config_path.exists():
-        sys.stderr.write(f'Cannot access config file: {config_path.absolute()}\n')
-        parser.print_help()
-        exit(1)
+    if not args.config_file:
+        for cp in config_paths:
+            if Path(cp).exists():
+                args.config_file = cp
+                break
+        else:
+            sys.stderr.write(f'Cannot access config file: {config_path.absolute()}\n')
+            parser.print_help()
+            exit(1)
 
-    config = WuzeiConfig(config_path)
+    config_path = Path(args.config_file)
+    try:
+        config = WuzeiConfig(config_path)
+    except (ValueError, KeyError):
+        sys.stderr.write(f'Cannot parse config file: {config_path.absolute()}\n')
+        raise
 
     print('Parsed Config:')
     print('==============')
